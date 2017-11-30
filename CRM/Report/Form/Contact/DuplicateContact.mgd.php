24bcf7313ca72680502f44c974aa73157ab4d1cf
Pradeep Nayak <pradpnayak@gmail.com>
2017-11-30 14:13:16 +0530

    initial commit

---
+++ CRM/Report/Form/Contact/DuplicateContact.mgd.php
@@ -0,0 +1,21 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Report_Form_Contact_DuplicateContact',
+    'entity' => 'ReportTemplate',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'DuplicateContact',
+      'description' => 'DuplicateContact (biz.jmaconsulting.duplicatecontactreport)',
+      'class_name' => 'CRM_Report_Form_Contact_DuplicateContact',
+      'report_url' => 'contact/duplicatecontact',
+      'component' => '',
+    ),
+  ),
+);
